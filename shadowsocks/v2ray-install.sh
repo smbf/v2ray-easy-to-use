b790cdedc5c6e17d401c6a819282eae2e010d2b1
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-02-14 01:04:45 +0800

    Update v2ray-install.sh

--- shadowsocks/v2ray-install.sh
+++ shadowsocks/v2ray-install.sh
@@ -27,11 +27,11 @@ function checkos(){
 
 function checkenv(){
     if [[ $OS = "centos" ]]; then
-	    yum upgrade -y
+        yum upgrade -y
         yum update -y
         yum install wget curl ntpdate -y
     else
-	    apt-get -y upgrade
+        apt-get -y upgrade
         apt-get -y update
         apt-get -y install wget curl ntpdate
     fi
@@ -43,19 +43,19 @@ function install_ssmkcp(){
     checkos
     checkenv
     ntpdate time.nist.gov
-    bash <(curl https://shadowsocks.v2ray-install.ml/install-release.sh)
+    bash <(curl https://raw.githubusercontent.com/1715173329/v2ray-easy-to-use/master/shadowsocks/install-release.sh)
     rm -rf "/etc/v2ray/config.json"
-    wget -qO /etc/v2ray/config.json "https://shadowsocks.v2ray-install.ml/config.json" 
+    wget -qO /etc/v2ray/config.json "https://raw.githubusercontent.com/1715173329/v2ray-easy-to-use/master/shadowsocks/config.json" 
     echo -e "请输入Shadowsocks的连接密码："
     read password
-	sed -i "s/sspwd/${password}/g" "/etc/v2ray/config.json"
-	Address=$(curl https://ipinfo.io/ip)
+    sed -i "s/sspwd/${password}/g" "/etc/v2ray/config.json"
+    Address=$(curl https://ipinfo.io/ip)
     service v2ray restart
-	clear
+    clear
     echo -e "您的连接信息如下："
-	echo -e "服务器地址：${Address}"
-	echo -e "服务器端口：8080"
-	echo -e "密码：${password}"
-	echo -e "加密方式：aes-256-gcm"
+    echo -e "服务器地址：${Address}"
+    echo -e "服务器端口：8080"
+    echo -e "密码：${password}"
+    echo -e "加密方式：aes-256-gcm"
 }
     install_ssmkcp
