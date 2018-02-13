8c33ac1153bc94c52340413a05eceb85210e2db7
1715173329 <22235437+1715173329@users.noreply.github.com>
2018-02-14 00:34:14 +0800

    Add files via upload

---
+++ http-dynport/v2ray-reinstall.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# Make sure only root can run our script
+function rootness(){
+    if [[ $EUID -ne 0 ]]; then
+       echo "Error:This script must be run as root,please run 'sudo su' first." 1>&2
+       exit 1
+    fi
+
+}
+ 
+ 
+function checkos(){
+    if [ -f /etc/redhat-release ];then
+        OS='centos'
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='debian'
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS='ubuntu'
+    else
+        echo "Not support OS, Please change OS and retry!"
+        exit 1
+    fi
+}
+
+
+function checkenv(){
+    if [[ $OS = "centos" ]]; then
+	    yum upgrade -y
+		yum update -y
+        yum install wget curl ntpdate -y
+    else
+	    apt-get -y upgrade
+        apt-get -y update
+        apt-get -y install wget curl ntpdate
+    fi
+}
+
+
+function reinstall_v2ray(){
+    checkos
+    rootness
+	checkenv
+	bash <(curl https://http-dynport.v2ray-install.ml/v2ray-uninstall.sh)
+	bash <(curl https://http-dynport.v2ray-install.ml/v2ray-install.sh)
+}
+    reinstall_v2ray
---
+++ http-dynport/v2ray-uninstall.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# Make sure only root can run our script
+function rootness(){
+    if [[ $EUID -ne 0 ]]; then
+       echo "Error:This script must be run as root,please run 'sudo su' first." 1>&2
+       exit 1
+    fi
+
+}
+ 
+ 
+function checkos(){
+    if [ -f /etc/redhat-release ];then
+        OS='centos'
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='debian'
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS='ubuntu'
+    else
+        echo "Not support OS, Please change OS and retry!"
+        exit 1
+    fi
+}
+
+
+function uninstall_v2ray(){
+    rootness
+    checkos
+    service v2ray stop
+    update-rc.d -f v2ray remove
+    systemctl disable v2ray
+    rm -rf /etc/v2ray
+    rm -rf /usr/bin/v2ray
+    rm -rf /var/log/v2ray
+    rm -rf /lib/systemd/system/v2ray.service
+    rm -rf /etc/init.d/v2ray
+	clear
+    echo -e "Uninstall is completed, thank you for your use!" 
+}
+    uninstall_v2ray
